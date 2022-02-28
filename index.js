5c504ba249a768447eae7d5f1a7a0cfba89bf983
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-02-28 23:15:53 +0600

    html and css basic structure is  done

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * API link and example for this task....
+ * 
+ * URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
+
+ * Example: https://openapi.programming-hero.com/api/phones?search=iphone
+
+ * Phone detail url:
+ * URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+ * Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+ */
