608f6d287da3a8002f20eb57780264c16229cc32
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-03-01 13:03:37 +0600

    details show on modal is done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const showSearchResult = (data) => {
 
     // clear the pervious content on the page 
     document.getElementById('mobile-not-found-message').style.display = 'none';
-    searchResultShow.textContent='';
+    searchResultShow.textContent = '';
 
     if (data.status == true) {
         const dataArray = data.data;
@@ -31,7 +31,7 @@ const showSearchResult = (data) => {
     
                             <!-- Button trigger modal and showing more details -->
                             <div class="text-center">
-                                <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">See
+                                <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails('${item.slug}')">See
                                     Details</a>
                             </div>
                         </div>
@@ -43,4 +43,57 @@ const showSearchResult = (data) => {
         document.getElementById('mobile-not-found-message').style.display = 'block';
     }
 
+}
+
+
+// show details on page after it selecting 
+const showDetails = (id) =>{
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+
+    // api load
+    fetch(url)
+    .then(res =>res.json())
+    .then(data=>showDetailsOnPage(data));
+}
+
+const showDetailsOnPage = (data) => {
+    console.log(data);
+    const maindiv = document.getElementById('product-page-details');
+
+    maindiv.textContent='';
+    const div = document.createElement('div');
+
+    if(data.status == true){
+
+  
+    const mobileData = data.data;
+
+    div.innerHTML=`<div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <div class="d-flex m-2 justify-content-center align-items-center ">
+    <div class="card border-0 shadow-none  w-50">
+    <img src="${mobileData.image}" class=" img-fluied rounded-start" alt="...">
+        
+    </div>
+    <div class="ms-5 w-50">
+        <h3  >Model: ${mobileData.name}</h3>
+        <h4>ReleaseData: ${mobileData.releaseDate}</h4>
+        <p  ><span class='h4'>Brand:</span> ${mobileData.brand}</p>
+        <p ><span class='h3'>Review: </span> <span class='text-warning'> <i class="fa-solid fa-star "></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> </span> </p>
+       
+    </div>
+  </div>
+    </div>
+  
+  </div>
+  `
+  maindiv.appendChild(div);
+
+}else{
+    console.log('mobile details is not founded');
+}
 }
